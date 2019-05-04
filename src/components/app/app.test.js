1196ed68e506b0d466ed1a4e70ce0542fd1c6c89
Mikhail <misha1247@mail.ru>
2019-05-04 18:14:24 +0300

    Add e2e test for card title link

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import App from './app';
 
+import App from './app.jsx';
 const movieTitlesCollection = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`, `Aviator`];
 
-it(`App renders correctly`, () => {
-  const tree = renderer
-    .create(<App
-      movieTitles={movieTitlesCollection}
-    />)
-    .toJSON();
+describe(`AppComponent`, () => {
+  it(`should render AppComponent correctly`, () => {
+    const tree = renderer
+      .create(<App
+        movieTitles={movieTitlesCollection}
+      />)
+      .toJSON();
 
-  expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot();
+  });
 });
