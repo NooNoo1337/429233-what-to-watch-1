be70fb82196c64788d4162dda57ceca25657330b
Mikhail <misha1247@mail.ru>
2019-05-07 11:38:50 +0300

    Add mocks for film cards

---
+++ src/mocks/films.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    genre: ``,
+    title: `Fantastic Beasts`,
+    src: ``,
+  },
+  {
+    genre: ``,
+    title: `Bohemian Rhapsody`,
+    src: ``,
+  },
+  {
+    genre: ``,
+    title: `Macbeth`,
+    src: ``,
+  },
+  {
+    genre: ``,
+    title: `Aviator`,
+    src: ``,
+  },
+  {
+    genre: ``,
+    title: `Filth`,
+    src: ``,
+  },
+];
