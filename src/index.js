ebf1865df6b230a827bdb15ea15955dfbf0bc829
Mikhail <misha1247@mail.ru>
2019-05-28 14:41:46 +0300

    add recompose and applied middleWare

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {compose} from 'recompose';
 
 import {rootReducer} from '@/reducer/reducer';
 import App from '@/components/app/app.jsx';
+import configureAPI from './api.js';
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const api = configureAPI((...args) => store.dispatch(...args));
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
+
+// thunk.withExtraArgument(api);
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById(`root`));
