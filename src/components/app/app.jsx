37339418a348a9e7eda1657ba00f0cb5443cf23d
Mikhail <misha1247@mail.ru>
2019-06-01 15:27:05 +0300

    add conditional rendering of SignIn component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,15 +4,17 @@ import {connect} from 'react-redux';
 
 import FilmsList from '../../components/films-list/films-list.jsx';
 import GenreList from '../../components/genre-list/genre-list.jsx';
+import SignIn from '../../components/sign-in/sign-in.jsx';
 import withActiveCard from '../../hocs/with-active-card/with-active-card.js';
 import {ActionCreators} from '../../reducer/data/data.js';
+
 import {getUniqGenres, getActiveGenre, getFilteredFilms} from '../../reducer/data/selectors.js';
 
 const FilmListWithActiveCard = withActiveCard(FilmsList);
 
 class App extends Component {
   render() {
-    const {films, genres, activeGenre, onGenreChange, onCardTitleClick} = this.props;
+    const {films, genres, activeGenre, onGenreChange, onCardTitleClick, isAuthorizationRequired} = this.props;
     return (
       <>
         <div className="visually-hidden">
@@ -47,85 +49,90 @@ class App extends Component {
           </svg>
         </div>
 
-        <section className="movie-card">
-          <div className="movie-card__bg">
-            <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel"/>
-          </div>
-
-          <h1 className="visually-hidden">WTW</h1>
-
-          <header className="page-header movie-card__head">
-            <div className="logo">
-              <a className="logo__link">
-                <span className="logo__letter logo__letter--1">W</span>
-                <span className="logo__letter logo__letter--2">T</span>
-                <span className="logo__letter logo__letter--3">W</span>
-              </a>
-            </div>
-
-            <div className="user-block">
-              <div className="user-block__avatar">
-                <img src="img/avatar.jpg" alt="User avatar" width="63" height="63"/>
-              </div>
-            </div>
-          </header>
-
-          <div className="movie-card__wrap">
-            <div className="movie-card__info">
-              <div className="movie-card__poster">
-                <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327"/>
+        {isAuthorizationRequired ? (
+          <SignIn/>
+        ) : (
+          <>
+            <section className="movie-card">
+              <div className="movie-card__bg">
+                <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel"/>
               </div>
 
-              <div className="movie-card__desc">
-                <h2 className="movie-card__title">The Grand Budapest Hotel</h2>
-                <p className="movie-card__meta">
-                  <span className="movie-card__genre">Drama</span>
-                  <span className="movie-card__year">2014</span>
-                </p>
+              <h1 className="visually-hidden">WTW</h1>
 
-                <div className="movie-card__buttons">
-                  <button className="btn btn--play movie-card__button" type="button">
-                    <svg viewBox="0 0 19 19" width="19" height="19">
-                      <use xlinkHref="#play-s"></use>
-                    </svg>
-                    <span>Play</span>
-                  </button>
-                  <button className="btn btn--list movie-card__button" type="button">
-                    <svg viewBox="0 0 19 20" width="19" height="20">
-                      <use xlinkHref="#add"></use>
-                    </svg>
-                    <span>My list</span>
-                  </button>
+              <header className="page-header movie-card__head">
+                <div className="logo">
+                  <a className="logo__link">
+                    <span className="logo__letter logo__letter--1">W</span>
+                    <span className="logo__letter logo__letter--2">T</span>
+                    <span className="logo__letter logo__letter--3">W</span>
+                  </a>
+                </div>
+
+                <div className="user-block">
+                  <div className="user-block__avatar">
+                    <img src="img/avatar.jpg" alt="User avatar" width="63" height="63"/>
+                  </div>
+                </div>
+              </header>
+
+              <div className="movie-card__wrap">
+                <div className="movie-card__info">
+                  <div className="movie-card__poster">
+                    <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327"/>
+                  </div>
+
+                  <div className="movie-card__desc">
+                    <h2 className="movie-card__title">The Grand Budapest Hotel</h2>
+                    <p className="movie-card__meta">
+                      <span className="movie-card__genre">Drama</span>
+                      <span className="movie-card__year">2014</span>
+                    </p>
+
+                    <div className="movie-card__buttons">
+                      <button className="btn btn--play movie-card__button" type="button">
+                        <svg viewBox="0 0 19 19" width="19" height="19">
+                          <use xlinkHref="#play-s"></use>
+                        </svg>
+                        <span>Play</span>
+                      </button>
+                      <button className="btn btn--list movie-card__button" type="button">
+                        <svg viewBox="0 0 19 20" width="19" height="20">
+                          <use xlinkHref="#add"></use>
+                        </svg>
+                        <span>My list</span>
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-        </section>
+            </section>
+            <div className="page-content">
+              <section className="catalog">
+                <h2 className="catalog__title visually-hidden">Catalog</h2>
+                <GenreList genres={genres} activeGenre={activeGenre} onGenreChange={onGenreChange} />
+                <FilmListWithActiveCard films={films} onCardTitleClick={onCardTitleClick}/>
+                <div className="catalog__more">
+                  <button className="catalog__button" type="button">Show more</button>
+                </div>
+              </section>
 
-        <div className="page-content">
-          <section className="catalog">
-            <h2 className="catalog__title visually-hidden">Catalog</h2>
-            <GenreList genres={genres} activeGenre={activeGenre} onGenreChange={onGenreChange} />
-            <FilmListWithActiveCard films={films} onCardTitleClick={onCardTitleClick}/>
-            <div className="catalog__more">
-              <button className="catalog__button" type="button">Show more</button>
-            </div>
-          </section>
+              <footer className="page-footer">
+                <div className="logo">
+                  <a className="logo__link logo__link--light">
+                    <span className="logo__letter logo__letter--1">W</span>
+                    <span className="logo__letter logo__letter--2">T</span>
+                    <span className="logo__letter logo__letter--3">W</span>
+                  </a>
+                </div>
 
-          <footer className="page-footer">
-            <div className="logo">
-              <a className="logo__link logo__link--light">
-                <span className="logo__letter logo__letter--1">W</span>
-                <span className="logo__letter logo__letter--2">T</span>
-                <span className="logo__letter logo__letter--3">W</span>
-              </a>
+                <div className="copyright">
+                  <p>© 2019 What to watch Ltd.</p>
+                </div>
+              </footer>
             </div>
-
-            <div className="copyright">
-              <p>© 2019 What to watch Ltd.</p>
-            </div>
-          </footer>
-        </div>
+          </>
+        )}
       </>
     );
   }
@@ -162,6 +169,7 @@ const mapStateToProps = (state, ownProps) => {
     films: getFilteredFilms(state),
     genres: getUniqGenres(state),
     activeGenre: getActiveGenre(state),
+    isAuthorizationRequired: state[`USER`].isAuthorizationRequired,
   });
 };
 
